d17d552499d78802d0ed7f3cbeee40d09a0f5d9d
Christian Pinto <pintochristian2@gmail.com>
2019-04-15 02:43:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   # your code here
-  i = []
-  array.each do |count|
-    i = array[count] ** 2
-    puts i
+  arr = []
+  array.each do { |i| arr << i ** 2 }
+    arr
   end
 end
